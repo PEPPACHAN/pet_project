197cd2fcf3907fc76b5c63b74e0c8de3c56545f6
PEPPACHAN <PEPPACHAN@users.noreply.github.com>
2023-09-07 12:50:29 +0300

    add coment

--- registration/models.py
+++ registration/models.py
@@ -7,7 +7,7 @@ class Registered_Users(models.Model):
     password = models.CharField(max_length=255)
     registration_date = models.DateTimeField(auto_now=True, auto_now_add=False)
     user_image = models.ImageField(default="media/default.png", upload_to="media/user_media")
-    last_login = models.DateTimeField(auto_now=False, auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=False, auto_now_add=True)  # reserved for login function
 
     class Meta:
         ordering = ["username"]

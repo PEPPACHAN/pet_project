8db160f7a178c0b8bb3ca54a278ec22f57a792cf
PEPPACHAN <PEPPACHAN@users.noreply.github.com>
2023-09-13 12:07:37 +0300

    add new db inside new app

--- product_sell/models.py
+++ product_sell/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from registration.models import *
 
-# Create your models here.
+
+class product_sell(models.Model):
+    username = models.ForeignKey(Registered_Users, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=150)
+    sell_products = models.TextField()  # Create textfield and fill it by json data
+    # Create condition check exist user or not
+
+    class Meta:
+        ordering = ["username"]
+
+    def __str__(self):
+        return self.username, self.product_name, self.sell_products
